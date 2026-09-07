ff766eb8ee48ed0220c8fd8982f8cf7faedc01b1
MousaXD <101021011+MousaXD@users.noreply.github.com>
2026-09-07 20:09:15 +0400

    feat: wire exact microscope jump controls

--- android/app/src/main/java/com/framescope/app/MainActivity.kt
+++ android/app/src/main/java/com/framescope/app/MainActivity.kt
@@ -47,8 +47,11 @@ class MainActivity : ComponentActivity() {
                     },
                     onCancelInspection = viewModel::cancelInspection,
                     onDismissError = viewModel::clearError,
-                    onPreviousFrame = { viewModel.stepMicroscope(-1) },
-                    onNextFrame = { viewModel.stepMicroscope(1) },
+                    onStepMicroscope = viewModel::stepMicroscope,
+                    onJumpMicroscopeFrame = viewModel::jumpMicroscopeFrame,
+                    onJumpMicroscopeTimestampUs = { timestampUs ->
+                        viewModel.jumpMicroscopeTimestampUs(timestampUs)
+                    },
                 )
             }
         }
