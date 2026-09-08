1cacf00ec1835940a66415b755c59bf931b68970
MousaXD <101021011+MousaXD@users.noreply.github.com>
2026-09-08 05:54:59 +0400

    fix: use Compose foundation keyboard options

--- android/app/src/main/java/com/framescope/app/ui/BatchExportOverlay.kt
+++ android/app/src/main/java/com/framescope/app/ui/BatchExportOverlay.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -27,7 +28,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.framescope.app.data.BatchExportRequest
