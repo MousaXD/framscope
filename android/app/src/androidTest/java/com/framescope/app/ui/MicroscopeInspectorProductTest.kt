46b960da3f90064cc66ae5b83e782795d16a84ff
MousaXD <101021011+MousaXD@users.noreply.github.com>
2026-09-09 08:08:20 +0400

    Fix Inspector Compose test assertion import

--- android/app/src/androidTest/java/com/framescope/app/ui/MicroscopeInspectorProductTest.kt
+++ android/app/src/androidTest/java/com/framescope/app/ui/MicroscopeInspectorProductTest.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
