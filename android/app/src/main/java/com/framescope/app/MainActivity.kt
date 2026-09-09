c1b9a67b3c178c290039840a99bf56375c376ca8
MousaXD <101021011+MousaXD@users.noreply.github.com>
2026-09-09 08:08:21 +0400

    Restore Compose setContent import

--- android/app/src/main/java/com/framescope/app/MainActivity.kt
+++ android/app/src/main/java/com/framescope/app/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.setContent
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
